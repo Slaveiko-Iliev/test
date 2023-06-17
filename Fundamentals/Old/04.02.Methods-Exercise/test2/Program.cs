15cad8548f350f91b286f0f36716ebd537e2d9a8
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-06-17 17:29:07 +0300

    up

---
+++ Fundamentals/Old/04.02.Methods-Exercise/test2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(0%2);
+        }
+    }
+}
