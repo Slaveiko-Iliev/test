9c26630a257479e4a99ac1f76a81a832c929fcef
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-06-30 06:13:08 +0300

    up

---
+++ Fundamentals/Old/06.02.ObjectsAndClasses-Exercise/07.OrderByAge/Program.cs
@@ -0,0 +1,37 @@
+﻿string input = string.Empty;
+
+List<Person> persons = new List<Person>();
+
+while ((input = Console.ReadLine()) != "End")
+{
+    string[] persoInfo = input.Split();
+
+    string name = persoInfo[0];
+    string id = persoInfo[1];
+    int age = int.Parse(persoInfo[2]);
+
+    Person person = new Person(name, id, age);
+    persons.Add(person);
+}
+
+persons = persons.OrderBy(x => x.Age).ToList();
+
+foreach (Person person in persons)
+{
+    Console.WriteLine($"{person.Name} with ID: {person.Id} is {person.Age} years old.");
+}
+
+
+public class Person
+{
+    public Person(string name, string id, int age)
+    {
+        Name = name;
+        Id = id;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public int Age { get; set; }
+}
