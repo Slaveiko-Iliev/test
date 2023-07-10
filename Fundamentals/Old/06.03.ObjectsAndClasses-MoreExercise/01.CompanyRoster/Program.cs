f6ed111405ad38499818dc6d912b7cd2a26d916e
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-07-10 19:36:41 +0300

    up

--- Fundamentals/Old/06.03.ObjectsAndClasses-MoreExercise/01.CompanyRoster/Program.cs
+++ Fundamentals/Old/06.03.ObjectsAndClasses-MoreExercise/01.CompanyRoster/Program.cs
@@ -1,34 +1,36 @@
-﻿int number = int.Parse(Console.ReadLine());
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int number = int.Parse(Console.ReadLine());
 
 List<Employee> employees = new List<Employee>();
 
 for (int i = 0; i < number; i++)
 {
-    string[] input = Console.ReadLine()
-        .Split();
+string[] input = Console.ReadLine()
+    .Split();
 
-    string name = input[0];
-    double salary = double.Parse(input[1]);
-    string department = input[2];
+string name = input[0];
+double salary = double.Parse(input[1]);
+string department = input[2];
 
-    Employee employee = new Employee(name, salary, department);
-    employees.Add(employee);
+Employee employee = new Employee(name, salary, department);
+employees.Add(employee);
 }
 
-List <double> salaryInDepartment = new List<double>();
-Dictionary <string, List<double>> departments = new Dictionary <string, List<double>>();
+List<double> salaryInDepartment = new List<double>();
+Dictionary<string, List<double>> departments = new Dictionary<string, List<double>>();
 
 foreach (Employee employee in employees)
 {
-    string newDepartment = employee.Department;
-    double currentSalary = employee.Salary;
+string newDepartment = employee.Department;
+double currentSalary = employee.Salary;
 
-    if (!departments.ContainsKey(newDepartment))
-    {
-        departments[newDepartment] = new List<double>();
-    }
+if (!departments.ContainsKey(newDepartment))
+{
+departments[newDepartment] = new List<double>();
+}
 
-    departments[newDepartment].Add(currentSalary);
+departments[newDepartment].Add(currentSalary);
 }
 
 string departmentMaxAverage = departments.OrderByDescending(x => x.Value.Average()).First().Key;
@@ -36,7 +38,7 @@ string departmentMaxAverage = departments.OrderByDescending(x => x.Value.Average
 Console.WriteLine($"Highest Average Salary: {departmentMaxAverage}");
 foreach (Employee employee in employees.Where(x => x.Department == departmentMaxAverage).OrderByDescending(x => x.Salary))
 {
-    Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
+Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
 }
 
 
