fd4db6eef12c526ad0edb33e1b409e6056947319
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-07-10 13:26:10 +0300

    up

---
+++ Fundamentals/Old/06.03.ObjectsAndClasses-MoreExercise/03.SpeedRacing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Schema;
+
+//"<Model> <FuelAmount> <FuelConsumptionFor1km>". All cars start at 0 kilometers traveled.
+
+int number = int.Parse(Console.ReadLine());
+
+Car cars = new Car();
+
+public class Car
+{
+    public Car (string model, double fuelAmount, double fuelRate)
+    {
+        Model = model;
+        FuelAmount = fuelAmount;
+        FuelRate = fuelRate;
+        TraveledDistance = 0;
+    }
+
+
+    public string Model { get; set; }
+    public double FuelAmount { get; set; }
+    public double FuelRate { get; set; }
+    public double TraveledDistance { get; }
+}
