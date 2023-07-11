7521110f9cdb1b44f754171ba57f87942e152f39
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-07-11 06:36:30 +0300

    up

--- Fundamentals/Old/06.03.ObjectsAndClasses-MoreExercise/03.SpeedRacing/Program.cs
+++ Fundamentals/Old/06.03.ObjectsAndClasses-MoreExercise/03.SpeedRacing/Program.cs
@@ -1,14 +1,42 @@
-﻿using System.Xml.Schema;
+﻿using System.Collections.Generic;
+using System.Xml.Schema;
 
-//"<Model> <FuelAmount> <FuelConsumptionFor1km>". All cars start at 0 kilometers traveled.
 
 int number = int.Parse(Console.ReadLine());
 
-Car cars = new Car();
+List<Car> cars = new List<Car>();
+
+for (int i = 0; i < number; i++)
+{
+    string[] currenrCar = Console.ReadLine()
+        .Split();
+
+    string model = currenrCar[0];
+    double fuelAmount = double.Parse(currenrCar[1]);
+    double fuelRate = double.Parse(currenrCar[2]);
+
+    Car car = new Car(model, fuelAmount, fuelRate);
+    cars.Add(car);
+}
+
+string input = string.Empty;
+
+while ((input = Console.ReadLine()) != "End")
+{
+    string[] command = input.Split();
+    string model = command[1];
+    double amountOfKm = double.Parse(command[2]);
+
+    cars.Find(x => x.Model == model).Drive(amountOfKm);
+}
+
+Console.Write(String.Join(Environment.NewLine, cars));
+
+
 
 public class Car
 {
-    public Car (string model, double fuelAmount, double fuelRate)
+    public Car(string model, double fuelAmount, double fuelRate)
     {
         Model = model;
         FuelAmount = fuelAmount;
@@ -20,5 +48,23 @@ public class Car
     public string Model { get; set; }
     public double FuelAmount { get; set; }
     public double FuelRate { get; set; }
-    public double TraveledDistance { get; }
+    public double TraveledDistance { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Model} {FuelAmount:f2} {TraveledDistance}";
+    }
+
+    public void Drive(double amountOfKm)
+    {
+        if (amountOfKm * FuelRate > FuelAmount)
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+        else
+        {
+            FuelAmount -= amountOfKm * FuelRate;
+            TraveledDistance += amountOfKm;
+        }
+    }
 }
---
+++ Fundamentals/Old/06.03.ObjectsAndClasses-MoreExercise/04.RawData/Program.cs
@@ -0,0 +1,77 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+List<Car> cars = new List<Car>(); //"<Model> <EngineSpeed> <EnginePower> <CargoWeight> <CargoType>
+
+for (int i = 0; i < number; i++)
+{
+    string[] carInfo = Console.ReadLine().Split();
+    string model = carInfo[0];
+    int engineSpeed = int.Parse(carInfo[1]);
+    int enginePower = int.Parse(carInfo[2]);
+    int cargoWeight = int.Parse(carInfo[3]);
+    string cargoType = carInfo[4];
+
+    Engine engine = new Engine(engineSpeed, enginePower);
+    Cargo cargo = new Cargo(cargoWeight, cargoType);
+    Car car = new Car(model, engine, cargo);
+
+    cars.Add(car);
+}
+
+string command = Console.ReadLine();
+
+if (command == "fragile")
+{
+    cars.FindAll(x => x.Cargo.CargoType == command);//.Where(x => x.Cargo.CargoWeight < 1000);
+}
+else if (command == "flamable")
+{
+    cars.FindAll(x => x.Cargo.CargoType == command).Where(x => x.Engine.EnginePower > 250);
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, cars));
+
+
+
+public class Car
+{
+    public Car (string model, Engine engine, Cargo cargo)
+    {
+        Model = model;
+        Engine = engine;
+        Cargo = cargo;
+    }
+
+    public override string ToString()
+    {
+        return $"{Model}";
+    }
+
+    public string Model { get; set; }
+    public Engine Engine { get; set; }
+    public Cargo Cargo { get; set; }
+}
+
+public class Engine
+{
+    public Engine(int engineSpeed, int enginePower)
+    {
+        EngineSpeed = engineSpeed;
+        EnginePower = enginePower;
+    }
+
+    public int EngineSpeed { get; set; }
+    public int EnginePower { get; set; }
+}
+
+public class Cargo
+{
+    public Cargo(int cargoWeight, string cargoType)
+    {
+        CargoWeight = cargoWeight;
+        CargoType = cargoType;
+    }
+
+    public int CargoWeight { get; set; }
+    public string CargoType { get; set; }
+}
