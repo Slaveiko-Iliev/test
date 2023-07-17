f3cb53e5c8aa25ac1aad7f26fc7b4b93c1473c03
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-07-17 06:46:35 +0300

    up

--- Fundamentals/Old/07.03.AssociativeArrays-MoreExercise/01.Ranking/Program.cs
+++ Fundamentals/Old/07.03.AssociativeArrays-MoreExercise/01.Ranking/Program.cs
@@ -17,7 +17,9 @@ while ((input = Console.ReadLine()) != "end of contests")
 
 }
 
-Dictionary<string, int> userInfo = new Dictionary<string, int>();
+Dictionary<string, int> userContests = new Dictionary<string, int>();
+
+Dictionary<string, Dictionary<string, int>> usersInfo = new Dictionary<string, Dictionary<string, int>>();
 
 while ((input = Console.ReadLine()) != "end of submissions")
 {
@@ -27,11 +29,43 @@ while ((input = Console.ReadLine()) != "end of submissions")
     string username = result[2];
     int points = int.Parse(result[3]);
 
-    if (!listOfContests.ContainsKey (contest) && listOfContests[contest] ==passwordForContest)
+    if (!listOfContests.ContainsKey(contest) || listOfContests[contest] != passwordForContest)
     {
-        if(!userInfo.ContainsKey(username))
-        {
-            userInfo.Add(username, points);
-        }
+        continue;
+    }
+
+    if (!usersInfo.ContainsKey(username))
+    {
+        usersInfo.Add(username, new Dictionary<string, int>());
+    }
+
+    if (!usersInfo[username].ContainsKey(contest))
+    {
+        usersInfo[username].Add(contest, points);
+    }
+
+    if (usersInfo[username].ContainsKey(contest) && usersInfo[username][contest] < points)
+    {
+        usersInfo[username][contest] = points;
     }
 }
+
+var bestCandidate = usersInfo.OrderByDescending(x => x.Value.Values.Sum()).First();
+
+Console.WriteLine($"Best candidate is {bestCandidate.Key} with total {bestCandidate.Value.Values.Sum()} points.");
+
+usersInfo = usersInfo.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value));
+
+Console.WriteLine("Ranking: ");
+
+foreach (var user in usersInfo)
+{
+    Console.WriteLine($"{user.Key}");
+
+    foreach (var contest in user.Value)
+    {
+        Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+    }
+}
+
+
---
+++ Fundamentals/Old/07.03.AssociativeArrays-MoreExercise/02.Judge/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+
+string input = string.Empty;
+
+Dictionary<string, Dictionary<string,int>> contests = new Dictionary<string, Dictionary<string, int>>();
+Dictionary<string, int> userPoints = new Dictionary<string, int>();
+
+while ((input = Console.ReadLine()) != "no more time")
+{
+    string[] userContest = input.Split(" -> ");
+
+    string username = userContest[0];
+    string contest = userContest[1];
+    int points = int.Parse(userContest[2]);
+
+
+
+    if (!contests.ContainsKey(contest))
+    {
+        contests.Add(contest, new Dictionary<string, int>());
+    }
+
+    if (!contests[contest].ContainsKey(username))
+    {
+        contests[contest].Add(username, points);
+    }
+    else
+    {
+        if (contests[contest][username] < points)
+        {
+            contests[contest][username] = points;
+        }
+    }
+
+    if (!userPoints.ContainsKey(username))
+    {
+        userPoints.Add(username, points);
+    }
+    else
+    {
+        userPoints[username] += points;
+    }
+}
+
+int counter = 0;
+
+foreach (var contest in contests)
+{
+    Console.WriteLine($"{contest.Key}: {contest.Value.Count} participants");
+
+    foreach (var username in contest.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+    {
+        counter ++;
+
+        Console.WriteLine($"{counter}. {username.Key} <::> {username.Value}");
+    }
+
+    counter = 0;
+}
+
+Console.WriteLine("Individual standings:");
+
+counter = 0;
+
+foreach (var user in userPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+{
+    counter ++;
+    Console.WriteLine($"{counter}. {user.Key} -> {user.Value}");
+}
