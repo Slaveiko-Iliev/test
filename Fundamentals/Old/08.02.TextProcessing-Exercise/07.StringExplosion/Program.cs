1cb773c419d673518997b7da95af4a8b4722bee9
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-07-29 20:48:42 +0300

    up

---
+++ Fundamentals/Old/08.02.TextProcessing-Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+int bombPower = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (bombPower > 0 && input[i] != '>')
+    {
+        input = input.Remove(i, 1);
+        bombPower--;
+        i--;
+    }
+    else if (input[i] == '>')
+    {
+        bombPower += int.Parse(input[i + 1].ToString());
+    }
+}
+Console.WriteLine(input);
