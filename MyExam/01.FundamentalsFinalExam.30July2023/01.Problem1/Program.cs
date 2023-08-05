1a1da29bb68824c2f1e1c6663c9f454d626e872f
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-08-05 15:05:48 +0300

    up

---
+++ MyExam/01.FundamentalsFinalExam.30July2023/01.Problem1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+string text = Console.ReadLine();
+
+string input = string.Empty;
+
+while ((input = Console.ReadLine()) != "Done")
+{
+    string[] commandInfo = input.Split();
+    string command = commandInfo[0];
+
+    if (command == "Change")
+    {
+        char ch = char.Parse(commandInfo[1]);
+        char replacement = char.Parse(commandInfo[2]);
+
+        text = text.Replace(ch, replacement);
+
+        Console.WriteLine(text);
+    }
+    else if (command == "Includes")
+    {
+        string substring = commandInfo[1];
+
+        bool IsContains = text.Contains(substring);
+        Console.WriteLine(IsContains);
+    }
+    else if (command == "End")
+    {
+        string substring = commandInfo[1];
+
+        bool IsEndWith = text.EndsWith(substring);
+        Console.WriteLine(IsEndWith);
+    }
+    else if (command == "Uppercase")
+    {
+        text = text.ToUpper();
+        Console.WriteLine(text);
+    }
+    else if (command == "FindIndex")
+    {
+        char ch = char.Parse(commandInfo[1]);
+        int index = text.IndexOf(ch);
+
+        if (index != -1)
+        {
+            Console.WriteLine(index);
+        }
+    }
+    else if (command == "Cut")
+    {
+        int startIndex = int.Parse(commandInfo[1]);
+        int count = int.Parse(commandInfo[2]);
+
+        string substring = text.Substring(startIndex, count);
+
+        Console.WriteLine(substring);
+    }
+}
---
+++ MyExam/01.FundamentalsFinalExam.30July2023/02.BossRush/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+int number = int.Parse(Console.ReadLine());
+
+string pattern = @"(?:\|)(?<bossName>[A-Z]{4,})(?:\|):(?:#)(?<title>[a-zA-Z]+ [a-zA-Z]+)(?:#)";
+
+for (int i = 0; i < number; i++)
+{
+    string bossInfo = Console.ReadLine();
+
+    Regex regex = new Regex(pattern);
+    
+
+    if (Regex.IsMatch(bossInfo, pattern))
+    {
+        Match match = regex.Match(bossInfo);
+        string bossName = match.Groups["bossName"].Value;
+        string title = match.Groups["title"].Value;
+
+        Console.WriteLine($"{bossName}, The {title}");
+        Console.WriteLine($">> Strength: {bossName.Length}");
+        Console.WriteLine($">> Armor: {title.Length}");
+    }
+    else
+    {
+        Console.WriteLine("Access denied!");
+    }
+}
