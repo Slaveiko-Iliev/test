4ad99be2980b17a8e92c6977e4775051261e7959
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-08-23 18:12:32 +0300

    up

---
+++ Fundamentals/Old/09.02.RegularExpressions-Exercise/05.NetherRealms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+
+string patternForSplit = @",\s*";
+string patternForHealth = @"[^0-9+\-*\/.]";
+
+//Regex findInfoForHealth = new Regex(patternForHealth);
+
+string[] namesOfDemons = Regex.Split(input, patternForSplit);
+
+Dictionary<string, List<double>> demons = new Dictionary<string, List<double>>();
+
+foreach (string name in namesOfDemons)
+{
+    double health = 0;
+    double damage = 0;
+
+    var healthInfo = Regex.Matches(name, patternForHealth)
+    .Cast<char>()
+    .ToArray();
+
+    //MatchCollection healthInfo = findInfoForHealth.Matches(name);
+    //var list = healthInfo.Cast<Match>().Select(match => match.Value).ToList();
+
+    //Console.WriteLine( string.Join("", list));
+
+    foreach (var match in healthInfo)
+    {
+        health += match;
+    }
+    ;
+}
