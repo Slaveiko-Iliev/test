2ac3862fb55451a071f0baa1efbfecc623cafd28
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-09-04 06:41:11 +0300

    up

---
+++ C# Advanced/01.02.StacksAndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int priceOfBullet = int.Parse(Console.ReadLine());
+int sizeOfGunBarrel = int.Parse(Console.ReadLine());
+int[] bulletsInfo = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();    
+int[] locksInfo = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int valueOfIntelligence = int.Parse(Console.ReadLine());
+
+Stack<int> bullets = new (bulletsInfo);
+Queue<int> locks = new (locksInfo);
+bool isThereAreLocks = false;
+int currentBarrel = sizeOfGunBarrel;
+
+if (locks.Count > 0)
+{
+    isThereAreLocks = true;
+}
+
+while (bullets.Count > 0)
+{
+    if (!isThereAreLocks)
+    {
+        Console.WriteLine($"{bullets.Count} bullets left. Earned ${valueOfIntelligence}");
+        break;
+    }
+
+    if (bullets.Pop() <= locks.Peek())
+    {
+        locks.Dequeue();
+        Console.WriteLine("Bang!");
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+
+    currentBarrel--;
+    valueOfIntelligence -= priceOfBullet;
+
+    if (locks.Count == 0)
+    {
+        isThereAreLocks = false;
+    }
+
+    if (bullets.Count == 0)
+    {
+        break;
+    }
+
+    if (currentBarrel == 0)
+    {
+        Console.WriteLine("Reloading!");
+
+        if (sizeOfGunBarrel >= bullets.Count)
+        {
+            currentBarrel += bullets.Count;
+        }
+        else
+        {
+            currentBarrel += sizeOfGunBarrel;
+        }
+    }
+}
+
+if (!isThereAreLocks && bullets.Count == 0)
+{
+    Console.WriteLine($"{bullets.Count} bullets left. Earned ${valueOfIntelligence}");
+}
+else if (isThereAreLocks)
+{
+    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+}
---
+++ C# Advanced/01.02.StacksAndQueues-Exercise/12.CupsAndBottles/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int[] cupsInfo = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+int[] bottlesInfo = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> cupsCapacity = new (cupsInfo);
+Stack<int> filledBottles = new (bottlesInfo);
+bool isThereCups = false;
+int wastedLittersOfWater = 0;
+int currentCup = 0;
+
+if (cupsCapacity.Count > 0)
+{
+    isThereCups = true;
+}
+
+while (filledBottles.Count > 0)
+{
+    if (cupsCapacity.Count == 0)
+    {
+        isThereCups = false;
+        Console.WriteLine($"Bottles: {string.Join(" ", filledBottles)}");
+        Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}");
+        break;
+    }
+
+    currentCup = cupsCapacity.Peek();
+
+    while (currentCup > 0)
+    {
+        currentCup -= filledBottles.Pop();
+
+        if (currentCup < 0)
+        {
+            wastedLittersOfWater += Math.Abs(currentCup);
+        }
+
+        if (filledBottles.Count == 0)
+        {
+            break;
+        }
+    }
+
+    cupsCapacity.Dequeue();
+}
+
+if (isThereCups)
+{
+    Console.WriteLine($"Cups: {string.Join(" ", cupsCapacity)}");
+    Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}");
+}
