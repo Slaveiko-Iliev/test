683e910c23c4830eaecae88f3ff8c7b40af1ecbd
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-10-06 15:08:15 +0300

    up

---
+++ C# Advanced/06.02.DefiningClasses-Exercise/09.P okemonTrainer/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.P_okemonTrainer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = string.Empty;
+            Dictionary<string, Trainer> trainers = new();
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                //"{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+            }
+        }
+    }
+}
---
+++ C# Advanced/06.02.DefiningClasses-Exercise/09.P okemonTrainer/Trainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _09.P_okemonTrainer
+{
+    public class Trainer
+    {
+        private string name;
+        private int numberOfBadges;
+        private List<Pokemon> pokemons;
+
+        public Trainer(string name, int numberOfBadges)
+        {
+            Name = name;
+            NumberOfBadges = numberOfBadges;
+            pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int NumberOfBadges { get; set; }
+    }
+}
