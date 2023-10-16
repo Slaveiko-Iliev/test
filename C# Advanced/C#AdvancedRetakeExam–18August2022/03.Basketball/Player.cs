d855935043d26ece29dfbd157ad8f7666d555bb9
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-10-16 16:06:14 +0300

    up

---
+++ C# Advanced/C#AdvancedRetakeExam–18August2022/03.Basketball/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Basketball
+{
+    public class Player
+    {
+        public Player(string name, string position, double rating, int games)
+        {
+            PlayerName = name;
+            Position = position;
+            Rating = rating;
+            Games = games;
+            Retired = false;
+        }
+
+        public string PlayerName { get; set; }
+        public string Position { get; set; }
+        public double Rating { get; set; }
+        public int Games { get; set; }
+        public bool Retired { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            
+            return $"-Player: {PlayerName}{Environment.NewLine}" +
+                $"--Position: {Position}{Environment.NewLine}" +
+                $"--Rating: {Rating}{Environment.NewLine}" +
+                $"--Games played:{Games}";
+        }
+    }
+}
