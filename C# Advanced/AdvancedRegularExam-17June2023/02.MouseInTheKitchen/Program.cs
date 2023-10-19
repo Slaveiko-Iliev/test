497fef92002423af532508c39f291695972afd45
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-10-19 19:31:48 +0300

    up

---
+++ C# Advanced/AdvancedRegularExam-17June2023/02.MouseInTheKitchen/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] matrixInfo = Console.ReadLine()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = matrixInfo[0];
+int cols = matrixInfo[1];
+
+char[,] cupboard = new char[rows, cols];
+int cheeseCount = 0;
+int mouseRow = -1;
+int mouseCol = -1;
+
+for (int i = 0; i < rows; i++)
+{
+    string currentRow = Console.ReadLine();
+
+    for (int j = 0; j < cols; j++)
+    {
+        cupboard[i, j] = currentRow[j];
+        if (cupboard[i, j] == 'C')
+        {
+            cheeseCount++;
+        }
+        else if (cupboard[i, j] == 'M')
+        {
+            mouseRow = i;
+            mouseCol = j;
+        }
+    }
+}
+
+string input = string.Empty;
+
