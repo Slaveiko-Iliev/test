ede5ff2a364cc0b1d540e0e9c3d817092c183697
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-10-26 15:16:35 +0300

    up

---
+++ Temp/NeedForSpeed/Car.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double _defaultFuelConsumption = 3;
+
+        public Car(int horsePower, double fuel) : base(horsePower, fuel) 
+        {
+
+        }
+    }
+}
