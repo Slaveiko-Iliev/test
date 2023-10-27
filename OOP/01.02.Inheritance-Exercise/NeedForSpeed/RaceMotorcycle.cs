f8d804fbbfb551f64834d3b0ce812122a4104ec7
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-10-27 18:40:25 +0300

    up

---
+++ OOP/01.02.Inheritance-Exercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double _defaultFuelConsumption = 8;
+
+        public RaceMotorcycle(int horsePower, double fuel) : base (horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption => _defaultFuelConsumption;
+    }
+}
---
+++ OOP/01.02.Inheritance-Exercise/NeedForSpeed/SportCar.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        private const double _defaultFuelConsumption = 10;
+
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+
+        }
+
+        public override double FuelConsumption => _defaultFuelConsumption;
+    }
+}
