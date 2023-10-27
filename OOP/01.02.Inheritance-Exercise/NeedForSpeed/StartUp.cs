abe6f140597ba98b981d674a046ca046ae46b6fd
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-10-27 09:55:36 +0300

    up

--- OOP/01.02.Inheritance-Exercise/NeedForSpeed/StartUp.cs
+++ OOP/01.02.Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -1,10 +1,26 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            Vehicle vehicle = new Vehicle(200, 100);
+            Car car = new Car(200, 100);
+            SportCar sportCar = new SportCar(200, 100);
+            RaceMotorcycle raceMotorcycle = new RaceMotorcycle(200, 100);
+
+            vehicle.Drive(10);
+            car.Drive(10);
+            sportCar.Drive(10);
+            raceMotorcycle.Drive(10);
+
+
+            Console.WriteLine(vehicle.Fuel);
+            Console.WriteLine(car.Fuel);
+            Console.WriteLine(sportCar.Fuel);
+            Console.WriteLine(raceMotorcycle.Fuel);
         }
     }
 }
