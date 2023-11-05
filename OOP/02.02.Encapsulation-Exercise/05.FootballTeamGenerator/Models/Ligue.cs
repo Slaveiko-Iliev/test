a2c816aa70b0a134352789c4adf6160c7c65fb46
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-11-05 09:51:32 +0200

    up

---
+++ OOP/02.02.Encapsulation-Exercise/05.FootballTeamGenerator/Models/Ligue.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.FootballTeamGenerator.Models
+{
+    public class Ligue
+    {
+        private List<Team> _teams;
+
+        public Ligue()
+        {
+            _teams = new List<Team>();
+        }
+
+        public void AddTeam(Team team)
+        {
+            if (!_teams.Contains(team))
+            {
+                _teams.Add(team);
+            }
+        }
+
+        public IReadOnlyList <Team> Teams => _teams;
+        public bool IsTeamExists(string teamName)
+        {
+            return _teams.Contains(_teams.FirstOrDefault(t => t.TeamName == teamName));
+        }
+    }
+}
---
+++ OOP/02.02.Encapsulation-Exercise/05.FootballTeamGenerator/Models/Team.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _05.FootballTeamGenerator.Models
+{
+    public class Team
+    {
+        private string _teamName;
+        private List<Player> _players;
+
+        public Team(string teamName)
+        {
+            TeamName = teamName;
+            _players = new List<Player>();
+        }
+
+        public string TeamName
+        {
+            get => _teamName;
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessage.EmptyName);
+                }
+                _teamName = value;
+            }
+        }
+        public double Rating => Math.Round(_players.Average(p => p.OverallSkillsLevel()));
+
+        public void AddPlayer(Player player)
+        {
+            if (!_players.Contains(player))
+            {
+                _players.Add(player);
+            }
+        }
+        public void RemovePlayer(string playerName)
+        {
+            Player player = _players.FirstOrDefault(p => p.PlayerName == playerName);
+
+
+            if (!_players.Contains(player))
+            {
+                throw new ArgumentException($"Player {playerName} is not in {TeamName} team.");
+            }
+            else
+            {
+                _players.Remove(player);
+            }
+        }
+
+        public double TeamRating(string teamName)
+        {
+            double rating = 0;
+
+            foreach (Player player in _players)
+            {
+                rating += player.OverallSkillsLevel();
+            }
+
+            return rating;
+        }
+    }
+}
