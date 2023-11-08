e31c7800e62d0bf880fbc1e8afae90d6d4be8ead
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-11-08 23:02:48 +0200

    up

---
+++ OOP/04.01.Polymorphism-Lab/02.Animals/Models/Cat.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals.Models
+{
+    public class Cat : Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+            this.name = name;
+            this.favouriteFood = favouriteFood;
+        }
+
+        public override string ExplainSelf() =>
+            $"{base.ExplainSelf()}" +
+            $"{Environment.NewLine}" +
+            $"MEEOW";
+    }
+}
---
+++ OOP/04.01.Polymorphism-Lab/02.Animals/Models/Dog.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals.Models
+{
+    public class Dog :Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+            this.name = name;
+            this.favouriteFood = favouriteFood;
+        }
+
+        public override string ExplainSelf() =>
+            $"{base.ExplainSelf()}" +
+            $"{Environment.NewLine}" +
+            $"DJAAF";
+    }
+}
