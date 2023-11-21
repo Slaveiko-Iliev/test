ebdd27c5263898ae48d1b5d39028c44610388881
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-11-21 14:31:01 +0200

    up

---
+++ OOP/05.01.ReflectionAndAttributesLab/01. Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"Class under investigation: {className}");
+
+            Type typeOfClass = Type.GetType(className);
+
+            foreach (var field in fields)
+            {
+                FieldInfo[] fieldsInfo = typeOfClass.GetFields((BindingFlags)60);
+
+                foreach (var fieldInfo in fieldsInfo)
+                {
+                    string fieldValue = (string)fieldInfo.GetValue(null);
+
+                    sb.AppendLine($"{fieldInfo.Name} = {fieldValue}");
+                }
+
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
