403da9866040fcf82d9d1a4fef76207aeed80254
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-11-23 14:28:58 +0200

    up

---
+++ OOP/05.02.ReflectionAndAttributesExercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,36 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandInfo = args.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string command = commandInfo[0];
+            string[] commandArguments = commandInfo.Skip(1).ToArray();
+
+            Type commandType = Assembly
+                .GetEntryAssembly()
+                .GetTypes().FirstOrDefault(t => t.Name == $"{command}Command");
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Command not found.");
+            }
+
+            ICommand commandInatance =
+                Activator.CreateInstance(commandType) as ICommand;
+
+            MethodInfo methodInfo = typeof(ICommand).GetMethod("Execute");
+
+            object result = methodInfo.Invoke(commandInatance, new object[] { commandArguments });
+
+            return result.ToString();
+        }
+    }
+}
---
+++ OOP/05.02.ReflectionAndAttributesExercise/CommandPattern/Core/Engine.cs
@@ -0,0 +1,34 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private ICommandInterpreter _commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            _commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                try
+                {
+                    string result = _commandInterpreter.Read(input);
+
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
