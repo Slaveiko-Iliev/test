4b0c89c3d1c971cd6194e637a3e7ad2d7e3de568
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-11-28 15:10:21 +0200

    up

--- OOP/06.02.UnitTestingExercises/01. Database_Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
+++ OOP/06.02.UnitTestingExercises/01. Database_Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
@@ -1,10 +1,60 @@
 namespace CarManager.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class CarManagerTests
     {
+        public const double fuelAmount = 0;
+        public const string make = "InitMake";
+        public const string model = "InitModel";
+        public const double fuelConsumption = 5;
+        public const double fuelCapacity = 50;
 
+        public Car car;
+
+        [SetUp]
+        public void Setup()
+        {
+            car = new(make, model, fuelConsumption, fuelCapacity);
+        }
+
+
+        [Test]
+        public void CreatingCar_ShouldWorkCorrectly()
+        {
+            Assert.IsNotNull(car);
+            Assert.AreEqual(fuelAmount, car.FuelAmount);
+            Assert.AreEqual(make, car.Make);
+            Assert.AreEqual(model, car.Model);
+            Assert.AreEqual(fuelConsumption, car.FuelConsumption);
+            Assert.AreEqual(fuelCapacity, car.FuelCapacity);
+        }
+
+        [TestCase("", model, fuelConsumption, fuelCapacity)]
+        public void UsingEmptyMake_ShouldThrowArgumentException(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => car = new(make, model, fuelConsumption, fuelCapacity));
+
+            Assert.AreEqual("Make cannot be null or empty!", exception.Message);
+        }
+
+        [TestCase(make, "", fuelConsumption, fuelCapacity)]
+        public void UsingEmptyModel_ShouldThrowArgumentException(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => car = new(make, model, fuelConsumption, fuelCapacity));
+
+            Assert.AreEqual("Model cannot be null or empty!", exception.Message);
+        }
+
+        [TestCase(make, model, 0, fuelCapacity)]
+        [TestCase(make, model, -5, fuelCapacity)]
+        public void UsingZeroOrNegativeFuelConsumption_ShouldThrowArgumentException(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentException>(() => car = new(make, model, fuelConsumption, fuelCapacity));
+
+            Assert.AreEqual("Fuel consumption cannot be zero or negative!", exception.Message);
+        }
     }
 }
