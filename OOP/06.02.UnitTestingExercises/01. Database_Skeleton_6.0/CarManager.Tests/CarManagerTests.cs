c9f2d44a78c4ec9ef884486058503f466fb1e91c
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-11-29 20:44:13 +0200

    up

--- OOP/06.02.UnitTestingExercises/01. Database_Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
+++ OOP/06.02.UnitTestingExercises/01. Database_Skeleton_6.0/CarManager.Tests/CarManagerTests.cs
@@ -5,6 +5,19 @@ namespace CarManager.Tests
     [TestFixture]
     public class CarManagerTests
     {
+        public Car car;
 
+        [SetUp]
+        public void SetUp()
+        {
+            car = new("Make1", "Model1", 5, 50);
+        }
+
+        [Test]
+        public void DefaultMethodCar_ShouldWorkCorrectly()
+        {
+            Assert.IsNotNull(car);
+            Assert.AreEqual(0, car.FuelAmount);
+        }
     }
 }
