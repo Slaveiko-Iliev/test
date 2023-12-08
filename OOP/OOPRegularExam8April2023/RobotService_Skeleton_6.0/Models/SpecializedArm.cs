55fc588e76fe9e98babc4d4220af571af042de91
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2023-12-08 15:31:15 +0200

    up

---
+++ OOP/OOPRegularExam8April2023/RobotService_Skeleton_6.0/Models/SpecializedArm.cs
@@ -0,0 +1,13 @@
+﻿namespace RobotService.Models
+{
+    public class SpecializedArm : Supplement
+    {
+        private const int _InterfaceStandard = 10045;
+        private const int _BatteryUsage = 10000;
+
+
+        public SpecializedArm() : base(_InterfaceStandard, _BatteryUsage)
+        {
+        }
+    }
+}
---
+++ OOP/OOPRegularExam8April2023/RobotService_Skeleton_6.0/Repositories/SupplementRepository.cs
@@ -0,0 +1,38 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RobotService.Repositories
+{
+    public class SupplementRepository : IRepository<ISupplement>
+    {
+        private List<ISupplement> _models;
+
+        public SupplementRepository()
+        {
+            _models = new List<ISupplement>();
+        }
+
+        public IReadOnlyCollection<ISupplement> Models() => _models;
+
+        public void AddNew(ISupplement model)
+        {
+            _models.Add(model);
+        }
+
+        public ISupplement FindByStandard(int interfaceStandard) => _models.FirstOrDefault(m => m.InterfaceStandard == interfaceStandard);
+
+        public bool RemoveByName(string typeName)
+        {
+            if (_models.Remove(_models.FirstOrDefault(m => m.GetType().Name == typeName)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
