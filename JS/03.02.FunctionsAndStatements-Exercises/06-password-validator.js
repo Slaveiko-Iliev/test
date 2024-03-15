cf7c5bb237ee67d2706d3a9989658f93c9b5c425
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-03-15 13:16:01 +0200

    up

--- JS/03.02.FunctionsAndStatements-Exercises/06-password-validator.js
+++ JS/03.02.FunctionsAndStatements-Exercises/06-password-validator.js
@@ -1,5 +1,13 @@
 function solve(input) {
     let checkCount = 0;
+
+    let checkLength = (input) => input.length >= 6 && input.length <= 10 ? true : console.log(`Password must be between 6 and 10 characters`);
+    let checkOnlyLettersAndDigits = (input) => input.match(/[^a-zA-Z1-9]/g) ? console.log(`Password must consist only of letters and digits`) : true;
+    let checkIfAtLeastTwoDigits = (input) => input
+    .split('')
+    .filter(char => Number(char))
+    .length >= 2 ? true : console.log (`Password must have at least 2 digits`);
+
     if(checkLength(input)){
         checkCount ++;
     }
@@ -14,34 +22,13 @@ function solve(input) {
         console.log(`Password is valid`);
     }
 
-    function checkLength(input) {
-        if (input.length >= 6 && input.length <= 10){
-            return true;
-        } else {
-            console.log(`Password must be between 6 and 10 characters`);
-        }
-    }
     
-    function checkOnlyLettersAndDigits(input) {
-        let isOnlyLettersAndDigits = true;
+
     
-        if (input.match(/[^a-zA-Z1-9]/g)){
-            console.log(`Password must consist only of letters and digits`);
-        } else {
-            return true;
-        }
-    }
     
-    function checkIfAtLeastTwoDigits(input) {
-        if (input.match(/[1-9]{2,}/g)){
-            return true;
-        } else {
-            console.log(`Password must have at least 2 digits`);
-        }
-    }
+    
+    
 }
-
-
 
 solve(`logIn`);
 solve(`MyPass123`);
