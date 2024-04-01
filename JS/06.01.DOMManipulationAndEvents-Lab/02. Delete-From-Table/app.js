de08d2785b023a5ba99900a3d7e9e9b106a1ee55
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-04-01 22:13:53 +0300

    up

--- JS/06.01.DOMManipulationAndEvents-Lab/02. Delete-From-Table/app.js
+++ JS/06.01.DOMManipulationAndEvents-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,22 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const tbodyTrElement = document.querySelectorAll('#customers tbody tr');
+    console.log(tbodyTrElement);
+    const inputElement = document.querySelector('input');
+    console.log(inputElement.value);
+    const resultElement = document.getElementById('result');
+    let isFound = false;
+
+    for (const row of Array.from(tbodyTrElement)) {
+        console.log(row);
+        if (inputElement.value === row[1]){
+            console.log(row[1]);
+            tbodyTrElement.remove(row);
+            isFound = true;
+            resultElement.textContent = `Deleted.`;
+        }
+    }
+    
+    if (!isFound){
+        resultElement.textContent = `Not found.`;
+    }
 }
