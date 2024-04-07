c2aff389f41fc1c3d6f78d7c89754fad74096e32
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-04-07 23:54:29 +0300

    up

--- JS/06.02.DOMManipulationAndEvents-Exercise/01. Sections/script.js
+++ JS/06.02.DOMManipulationAndEvents-Exercise/01. Sections/script.js
@@ -2,12 +2,18 @@ function create(words) {
    const contentElement = document.getElementById('content');
 
    for (const word of words) {
-      const divElement = document.createElement('div');
       const pElement = document.createElement('p');
       pElement.textContent = word;
+      pElement.style.display = 'none';
+      
+
+      const divElement = document.createElement('div');
       divElement.appendChild(pElement);
+
+      divElement.addEventListener('click', () => {
+         pElement.style.display = 'block';
+      });
+
       contentElement.appendChild(divElement);
    }
-
-   console.log(contentElement);
 }
