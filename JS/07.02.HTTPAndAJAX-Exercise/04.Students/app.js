b207433a01082c66e95f5f066ee16a156b80f69b
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-04-12 14:57:29 +0300

    up

--- JS/07.02.HTTPAndAJAX-Exercise/04.Students/app.js
+++ JS/07.02.HTTPAndAJAX-Exercise/04.Students/app.js
@@ -1,5 +1,49 @@
 function attachEvents() {
-  // TODO:
+  const baseURL = `http://localhost:3030/jsonstore/collections/students`;
+  extractsStudentsRecords();
+
+  const submitButtonElement = document.getElementById('submit');
+  const inputFirstNameElement = document.getElementById('firstName');
+  const inputLastNameElement = document.getElementById('lastName');
+  const inputFacultyNumberElement = document.getElementById('facultyNumber');
+  const inputGradeNameElement = document.getElementById('grade');
+  const resultTbodyElement = document.querySelector('#results tbody');
+
+  extractsStudentsRecords();
+
+  function extractsStudentsRecords() {
+    fetch(`${baseURL}`)
+    .then(studentsRecordsresponse => studentsRecordsresponse.json())
+    .then(data => {
+      const studentList = Object.values(data);
+
+      resultTbodyElement.innerHTML = ``;
+
+       for (const student of studentList) {
+        const newTrElement = document.createElement('tr');
+
+        const firstNameTdElement = document.createElement('td');
+        firstNameTdElement.textContent = student.firstName;
+        newTrElement.appendChild(firstNameTdElement);
+
+        const lastNameTdElement = document.createElement('td');
+        lastNameTdElement.textContent = student.lastName;
+        newTrElement.appendChild(lastNameTdElement);
+
+        const facultyNumberTdElement = document.createElement('td');
+        facultyNumberTdElement.textContent = student.facultyNumber;
+        newTrElement.appendChild(facultyNumberTdElement);
+
+        const gradeNameTdElement = document.createElement('td');
+        gradeNameTdElement.textContent = student.grade.toFixed(2);
+        newTrElement.appendChild(gradeNameTdElement);
+
+        resultTbodyElement.appendChild(newTrElement);
+       }
+    })
+    .catch(error => console.log('Something went wrong'));
+    
+  }
 }
 
 attachEvents();
