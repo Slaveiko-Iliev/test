96555afa6d4cd826c139565ce172b9762568d05d
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-04-26 23:06:25 +0300

    Update SQLQuery_1.sql

--- MS SQL/SQLQuery_1.sql
+++ MS SQL/SQLQuery_1.sql
@@ -326,5 +326,97 @@ INSERT INTO Occupancies (EmployeeId, DateOccupied, RateApplied)
             (3, '2002-10-13', 'Vip')
 
 
+
 --Task 16 Create SoftUni Database
 
+CREATE DATABASE [SoftUni];
+
+CREATE TABLE Towns (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+   [Name] NVARCHAR (50) NOT NULL
+)
+
+CREATE TABLE Addresses (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    AddressText NVARCHAR (100) NOT NULL,
+    TownId INT FOREIGN KEY REFERENCES [Towns](Id) NOT NULL
+)
+
+CREATE TABLE Departments (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    [Name] NVARCHAR (50) NOT NULL
+)
+
+Create TABLE Employees (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    FirstName NVARCHAR (50) NOT NULL,
+    MiddleName NVARCHAR (50),
+    LastName NVARCHAR (50) NOT NULL,
+    JobTitle NVARCHAR (20),
+    DepartmentId INT FOREIGN KEY REFERENCES [Departments](Id),
+    HireDate DATE,
+    Salary DECIMAL (6,2),
+    AddressId INT FOREIGN KEY REFERENCES [Addresses](Id) 
+)
+
+
+-- Task 18. Basic Insert
+
+
+INSERT INTO Towns (name)
+    VALUES ('Sofia'),
+            ('Plovdiv'),
+            ('Varna'),
+            ('Burgas')
+
+INSERT INTO Departments (name)
+    VALUES ('Engineering'),
+            ('Sales'),
+            ('Marketing'),
+            ('Software Development'),
+            ('Quality Assurance')
+
+INSERT INTO Employees (FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary)
+    VALUES  ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+            ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+            ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+            ('Georgi', 'Teziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+            ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.98)
+
+
+--Task 19 Basic Select All Fields
+
+SELECT * FROM Towns;
+SELECT * FROM Departments;
+SELECT * FROM Employees;
+
+
+-- Task 20 Basic Select All Fields and Order Them
+
+SELECT * FROM Towns ORDER BY [Name] asc;
+SELECT * FROM Departments ORDER BY [Name] asc;
+SELECT * FROM Employees ORDER BY [Salary] desc;
+
+
+-- Task 21 Basic Select Some Fields
+
+SELECT ([Name]) FROM Towns;
+SELECT ([Name])  FROM Departments;
+SELECT [FirstName], [LastName], [JobTitle], [Salary] FROM Employees;
+
+
+-- Task 22 Increase Employees Salary
+
+UPDATE Employees SET Salary = Salary * 1.1;
+SELECT Salary FROM Employees;
+
+
+-- Task 23 Decrease Tax Rate
+
+UPDATE Payments SET TaxRate = TaxRate * 0.97;
+SELECT TaxRate FROM Payments;
+
+-- Task 24 Delete All Records
+
+
+TRUNCATE TABLE Occupancies
