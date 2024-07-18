e18d9fa0d21b538757620e5f94284bb69f885f8b
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-07-18 17:07:10 +0300

    up

--- Entity Framework Core/07. JSON Processing/ProductShop/StartUp.cs
+++ Entity Framework Core/07. JSON Processing/ProductShop/StartUp.cs
@@ -10,9 +10,39 @@ namespace ProductShop
         {
             ProductShopContext context = new ProductShopContext();
 
-            string list = File.ReadAllText("../../../Datasets/products.json");
+            //Task1 string usersList = File.ReadAllText("../../../Datasets/users.json");
+            //Console.WriteLine(ImportUsers(context, usersList));
 
-            Console.WriteLine(ImportProducts(context, list)); ;
+            //Task2 string productsList = File.ReadAllText("../../../Datasets/products.json");
+            //Console.WriteLine(ImportProducts(context, productsList)); 
+
+            //Task3 string categoriesList = File.ReadAllText("../../../Datasets/categories.json");
+            //Console.WriteLine(ImportCategories(context, categoriesList));
+
+            string catProList = File.ReadAllText("../../../Datasets/categories-products.json");
+            Console.WriteLine(ImportCategoryProducts(context, catProList));
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoryProducts = JsonConvert.DeserializeObject<List<CategoryProduct>>(inputJson);
+            context.CategoriesProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoryProducts.Count}";
+        }
+
+        //Task 3
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<List<Category>>(inputJson);
+
+            categories.RemoveAll(c => c.Name == null);
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Count}";
         }
 
         //Task 2
@@ -20,7 +50,7 @@ namespace ProductShop
         {
             var products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
 
-            context.AddRange(products);
+            context.Products.AddRange(products);
             context.SaveChanges();
 
             return $"Successfully imported {products.Count}";
@@ -29,12 +59,12 @@ namespace ProductShop
         //Task 1
         public static string ImportUsers(ProductShopContext context, string inputJson)
         {
-            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            var users = JsonConvert.DeserializeObject<List<User>>(inputJson);
 
-            context.AddRange(users);
+            context.Users.AddRange(users);
             context.SaveChanges();
 
-            return $"Successfully imported {users.Length}";
+            return $"Successfully imported {users.Count}";
         }
     }
 }
