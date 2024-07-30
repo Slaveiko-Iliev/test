cbf038b88370301d5819fc0ddc5a23dfd886a241
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-07-30 09:16:39 +0300

    up

---
+++ Entity Framework Core/C# DB Advanced Regular Exam - 02 December 2023/Medicines/Data/Models/Medicine.cs
@@ -0,0 +1,34 @@
+﻿using Medicines.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicines.Data.Models
+{
+    public class Medicine
+    {
+        public Medicine()
+        {
+            PatientsMedicines = new HashSet<PatientMedicine>();
+        }
+
+        public int Id { get; set; }
+        [MaxLength(150)]
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        [MaxLength(100)]
+        public string Producer { get; set; } = null!;
+        [ForeignKey(nameof(Pharmacy))]
+        public int PharmacyId { get; set; }
+        public Pharmacy Pharmacy { get; set; } = null!;
+        public ICollection<PatientMedicine> PatientsMedicines { get; set; }
+    }
+}
+
