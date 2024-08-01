428cee0d16d436fc8a2b3a5bb3b2ca55c684155c
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-08-01 07:23:09 +0300

    up

--- Entity Framework Core/C# DB Advanced Regular Exam - 02 December 2023/Medicines/DataProcessor/Serializer.cs
+++ Entity Framework Core/C# DB Advanced Regular Exam - 02 December 2023/Medicines/DataProcessor/Serializer.cs
@@ -1,17 +1,50 @@
 ﻿namespace Medicines.DataProcessor
 {
     using Medicines.Data;
+    using Medicines.Data.Models;
+    using Medicines.DataProcessor.ExportDtos;
+    using System.Globalization;
+    using Trucks.Utilities;
 
     public class Serializer
     {
         public static string ExportPatientsWithTheirMedicines(MedicinesContext context, string date)
         {
-            throw new NotImplementedException();
+            bool isValidDate = DateTime.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime productionDate);
+            XmlHelper xmlHelper = new XmlHelper();
+
+            Patient[] tmp = context.Patients
+                .Where(p => p.PatientsMedicines.Any(pm => pm.Medicine.ProductionDate > productionDate))
+                .ToArray();
+
+            ExportPatientDto[] patientsToExport = tmp
+                .Select(p => new ExportPatientDto()
+                {
+                    Gender = p.Gender.ToString().ToLower(),
+                    Name = p.FullName,
+                    AgeGroup = p.AgeGroup.ToString(),
+                    Medicines = p.PatientsMedicines.Select(pm => new MedicineDto()
+                    {
+                        Name = pm.Medicine.Name,
+                        Price = pm.Medicine.Price.ToString("f2"),
+                        Producer = pm.Medicine.Producer,
+                        BestBefore = pm.Medicine.ExpiryDate,
+                        Category = pm.Medicine.Category.ToString()
+                    })
+                                                    .OrderByDescending(m => m.BestBefore)
+                                                    .ThenBy(m => m.Price)
+                                                    .ToArray()
+                })
+                .OrderByDescending(p => p.Medicines.Length)
+                .ThenBy(p => p.Name)
+                .ToArray();
+
+            return xmlHelper.Serialize(patientsToExport, "Patients");
         }
 
         public static string ExportMedicinesFromDesiredCategoryInNonStopPharmacies(MedicinesContext context, int medicineCategory)
         {
-            throw new NotImplementedException();
+
         }
     }
 }
